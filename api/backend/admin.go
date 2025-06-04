8e6ce01bb3434692be874f512dab5e6d79097d13
CodeMachine01 <peihuachen2001@gmail.com>
2025-06-05 01:39:25 +0800

    '管理后台admin的增删改查_加密盐_公共类库_业务逻辑判断'

---
+++ api/backend/admin.go
@@ -0,0 +1,49 @@
+package backend
+
+import (
+	"github.com/gogf/gf/v2/frame/g"
+)
+
+type RotationReq struct {
+	g.Meta `path:"/backend/rotation/add" tags:"Rotation" method:"post" summary:"You first rotation api"`
+
+	PicUrl string `json:"pic_url"    v:"required#图片链接不能为空" dc:"图片链接"`
+	Link   string `json:"link"    v:"required#跳转链接不能为空" dc:"跳转链接"`
+	Sort   int    `json:"sort" dc:"排序"`
+}
+type RotationRes struct {
+	//g.Meta `mime:"text/html" example:"string"`
+	//todo
+	RotationId int `json:"rotationId"`
+}
+
+type RotationDeleteReq struct {
+	g.Meta `path:"/backend/rotation/delete" method:"delete" tags:"轮播图" summary:"删除轮播图接口"`
+	Id     int `v:"min:1#请选择需要删除的轮播图" dc:"轮播图id"`
+}
+type RotationDeleteRes struct{}
+
+type RotationUpdateReq struct {
+	g.Meta `path:"/backend/rotation/update/{Id}" method:"post" tags:"轮播图" summary:"修改轮播图接口"`
+	Id     int    `json:"id"      v:"min:1#请选择需要修改的轮播图" dc:"轮播图Id"`
+	PicUrl string `json:"pic_url"    v:"required#图片链接不能为空" dc:"图片链接"`
+	Link   string `json:"link"    v:"required#跳转链接不能为空" dc:"跳转链接"`
+	Sort   int    `json:"sort" dc:"排序"`
+}
+type RotationUpdateRes struct {
+	Id int `json:"id"`
+}
+
+type RotationGetListCommonReq struct {
+	g.Meta `path:"/backend/rotation/list" method:"get" tags:"轮播图" summary:"查询轮播图列表接口"`
+	Sort   int `json:"sort"   in:"query" dc:"排序类型"`
+	CommonPaginationReq
+}
+type RotationGetListCommonRes struct {
+	//todo
+	//g.Meta `mime:"text/html" type:"string" example:"<html/>"`
+	List  interface{} `json:"list" description:"列表"`
+	Page  int         `json:"page" description:"分页码"`
+	Size  int         `json:"size" description:"分页数量"`
+	Total int         `json:"total" description:"数据总数"`
+}
