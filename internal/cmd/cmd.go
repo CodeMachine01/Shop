bc91f69a8edfc39064e44f262846fdc53da72cc1
CodeMachine01 <peihuachen2001@gmail.com>
2025-07-21 15:58:37 +0800

    优惠券全流程开发

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -57,6 +57,7 @@ var (
 						controller.File,     //从0到1实现文件入库
 						controller.Upload,   //实现可跨项目使用的文件上云工具类
 						controller.Category, //商品分类
+						controller.Coupon,   //商品优惠券管理
 					)
 				})
 			})
--- internal/logic/logic.go
+++ internal/logic/logic.go
@@ -8,6 +8,7 @@ import (
 	_ "Shop/internal/logic/admin"
 	_ "Shop/internal/logic/bizctx"
 	_ "Shop/internal/logic/category"
+	_ "Shop/internal/logic/coupon"
 	_ "Shop/internal/logic/data"
 	_ "Shop/internal/logic/file"
 	_ "Shop/internal/logic/login"
