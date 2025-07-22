9870da913c5e8c32c153f24f7cf8b7b34977a35b
CodeMachine01 <peihuachen2001@gmail.com>
2025-07-22 15:30:27 +0800

    商品规格管理

--- internal/logic/logic.go
+++ internal/logic/logic.go
@@ -12,6 +12,7 @@ import (
 	_ "Shop/internal/logic/data"
 	_ "Shop/internal/logic/file"
 	_ "Shop/internal/logic/goods"
+	_ "Shop/internal/logic/goods_options"
 	_ "Shop/internal/logic/login"
 	_ "Shop/internal/logic/middleware"
 	_ "Shop/internal/logic/permission"
---
+++ internal/model/goods_options.go
@@ -0,0 +1,47 @@
+package model
+
+import "Shop/internal/model/entity"
+
+// GoodsOptionsCreateUpdateBase 创建/修改内容基类
+type GoodsOptionsCreateUpdateBase struct {
+	GoodsId uint
+	PicUrl  string
+	Name    string
+	Price   uint
+	Stock   uint
+}
+
+// GoodsOptionsCreateInput 创建内容
+type GoodsOptionsCreateInput struct {
+	GoodsOptionsCreateUpdateBase
+}
+
+// GoodsOptionsCreateOutput 创建内容返回结果
+type GoodsOptionsCreateOutput struct {
+	Id uint `json:"id"`
+}
+
+// GoodsOptionsUpdateInput 修改内容
+type GoodsOptionsUpdateInput struct {
+	GoodsOptionsCreateUpdateBase
+	Id int
+}
+
+// GoodsOptionsGetListInput 获取内容列表
+type GoodsOptionsGetListInput struct {
+	Page int // 分页号码
+	Size int // 分页数量，最大50
+	Sort int // 排序类型
+}
+
+// GoodsOptionsGetListOutput 查询列表结果
+type GoodsOptionsGetListOutput struct {
+	List  []GoodsOptionsGetListOutputItem `json:"list" description:"列表"`
+	Page  int                             `json:"page" description:"分页码"`
+	Size  int                             `json:"size" description:"分页数量"`
+	Total int                             `json:"total" description:"数据总数"`
+}
+
+type GoodsOptionsGetListOutputItem struct {
+	entity.GoodsOptionsInfo
+}
